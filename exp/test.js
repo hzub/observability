8442f5d9081d2d0f5ab4a7500c9019612d9dd3a2
Hubert Zub <hubert.zub@gmail.com>
2023-09-24 08:47:28 +0200

    v2
    Signed-off-by: Hubert Zub <hubert.zub@gmail.com>

--- exp/test.js
+++ exp/test.js
@@ -1,29 +1,56 @@
+// class ApiService {
+//   async call() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/postz");
+//     if (!response.ok) {
+//       return Promise.reject(new Error("Failed to fetch <URL>"));
+//     }
+//     return response.json();
+//   }
+// }
+
+// function process() {
+//   const initiatorStack = new Error().stack;
+//   new ApiService().call().catch((error) => {
+//     // ?
+//     const completeStack = [
+//       error.stack,
+//       ...initiatorStack.split("\n").slice(1),
+//     ].join("\n");
+//     console.log(completeStack);
+//   });
+// }
+
+// document.querySelector("button").addEventListener("click", () => {
+//   process();
+// });
+
+// window.addEventListener("unhandledrejection", (x) => {
+//   console.log("@@@OY", x);
+// });
+
 class ApiService {
   async call() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/postz");
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     if (!response.ok) {
-      return Promise.reject(new Error("Failed to fetch <URL>"));
+      throw new Error(
+        "Failed to fetch https://jsonplaceholder.typicode.com/postz"
+      );
     }
     return response.json();
   }
 }
 
 function process() {
-  const initiatorStack = new Error().stack;
-  new ApiService().call().catch((error) => {
-    // ?
-    const completeStack = [
-      error.stack,
-      ...initiatorStack.split("\n").slice(1),
-    ].join("\n");
-    console.log(completeStack);
-  });
+  new ApiService()
+    .call()
+    .then((data) => {
+      // ...
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 document.querySelector("button").addEventListener("click", () => {
   process();
-});
-
-window.addEventListener("unhandledrejection", (x) => {
-  console.log("@@@OY", x);
 });
